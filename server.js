8ea1580321c6844129425bee36ec9783510b77ee
cathyseo <cathyseo.d@gmail.com>
2023-12-26 16:42:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 // Serve static files (CSS, JS, etc.)
 app.use(express.static('public'));
+app.use(express.json());
 
 // Route for the emoji selection page
 app.get('/', (req, res) => {
@@ -20,10 +21,15 @@ app.get('/story', (req, res) => {
 
 // Endpoint to handle OpenAI API requests
 app.post('/generate-story', async (req, res) => {
+    // Log the entire request body and headers
+    // console.log("Request Body:", req.body);
+    // console.log("Request Headers:", req.headers);
+
     try {
+        // Your existing code to handle the request
         const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-            prompt: req.body.prompt, // or construct your prompt based on request data
-            max_tokens: 150 // or any other parameters
+            prompt: req.body.prompt,
+            max_tokens: 150
         }, {
             headers: {
                 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
@@ -32,8 +38,8 @@ app.post('/generate-story', async (req, res) => {
 
         res.json(response.data);
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error occurred while calling OpenAI API');
+        console.error("Error occurred:", error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 });
 
